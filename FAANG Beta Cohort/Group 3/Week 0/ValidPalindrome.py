0f0c674b8a3b56f5c0296ca79a592ea694dffa6e
westcliff-coders-11072024 <w.fang.407@westcliff.edu>
2024-11-07 22:17:53 -0800

    Update ValidPalindrome.py

--- FAANG Beta Cohort/Group 3/Week 0/ValidPalindrome.py
+++ FAANG Beta Cohort/Group 3/Week 0/ValidPalindrome.py
@@ -1,1 +1,46 @@
-
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = len(s)
+        if l == 1:
+            return True
+        else:
+            s = s.lower()
+            i = 0
+            j = l - 1
+            c1 = s[i]
+            c2 = s[j]
+            f1 = c1.isalnum()
+            f2 = c2.isalnum()
+            result = True
+            while(i < j):
+                if f1 and f2:
+                    if c1 == c2:
+                        i = i + 1
+                        j = j - 1
+                        c1 = s[i]
+                        c2 = s[j]
+                        f1 = c1.isalnum()
+                        f2 = c2.isalnum()
+                        continue
+                    else:
+                        result = False
+                        break
+                elif f1 and not f2:
+                    j = j - 1
+                    c2 = s[j]
+                    f2 = c2.isalnum()
+                    continue
+                elif not f1 and f2:
+                    i = i + 1
+                    c1 = s[i]
+                    f1 = c1.isalnum()
+                    continue
+                else:
+                    i = i + 1
+                    j = j - 1
+                    c1 = s[i]
+                    c2 = s[j]
+                    f1 = c1.isalnum()
+                    f2 = c2.isalnum()
+                    continue
+            return result
