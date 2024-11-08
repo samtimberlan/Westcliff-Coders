25d4af551a581eb981c4123fc703fe0629f78b6b
j-kumar-3266-westcliff-edu <j.kumar.3266@westcliff.edu>
2024-11-07 23:21:26 -0800

    Create JitendraKumar.py

---
+++ FAANG Beta Cohort/Group 1/Week 0/JitendraKumar.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+		//Transpose
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+        //Reverse
+		for i in range(n):
+            for j in range(n // 2):
+                matrix[i][n - j - 1], matrix[i][j] = matrix[i][j], matrix[i][n - j - 1]
+
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        min_str = min(strs,key=len)
+        for i,c in enumerate(min_str):
+            for s in strs:
+                if s[i] != c:
+                    return min_str[:i]
+        return min_str
