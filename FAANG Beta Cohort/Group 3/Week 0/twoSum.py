d9d737cb1a770c046fef485f1a32c3ecbc4b94e5
westcliff-coders-11072024 <w.fang.407@westcliff.edu>
2024-11-07 21:46:12 -0800

    Update twoSum.py

--- FAANG Beta Cohort/Group 3/Week 0/twoSum.py
+++ FAANG Beta Cohort/Group 3/Week 0/twoSum.py
@@ -4,4 +4,14 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
-        
+
+    # found this method form the internet
+    def twoSum2(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+        listSize = len(nums)
+
+        for i in range(listSize):
+            needNum = target - nums[i]
+            if needNum in numMap:
+                return [numMap[needNum], i]
+            numMap[nums[i]] = i
