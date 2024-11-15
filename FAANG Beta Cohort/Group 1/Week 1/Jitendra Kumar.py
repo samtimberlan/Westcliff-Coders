46a894d49cc7df91d1ffe34deec8f9456ce63348
j-kumar-3266-westcliff-edu <j.kumar.3266@westcliff.edu>
2024-11-14 16:29:16 -0800

    Add files via upload

---
+++ FAANG Beta Cohort/Group 1/Week 1/Jitendra Kumar.py
@@ -0,0 +1,39 @@
+Odd Even Linked ist: https://leetcode.com/problems/odd-even-linked-list/description/
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+        odd = head
+        even = head.next
+        evenHead = even
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = evenHead
+        return head
+
+Linked List Cycle: https://leetcode.com/problems/linked-list-cycle-ii/description/
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        visited = set()
+        node = head
+        while node is not None:
+            if node in visited:
+                return node
+            else:
+                visited.add(node)
+                node = node.next
+        return None
