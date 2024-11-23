803d69510f0c35977a46d5b8a9da15b06857e5e5
Jingxi <jingxi0529@gmail.com>
2024-11-23 12:52:24 -0800

    solutions for leetcode 94 & 225

---
+++ FAANG Beta Cohort/Group 2/Week 2/Solutions'.py
@@ -0,0 +1,58 @@
+# 94. Binary Tree Inorder Traversal
+
+# Definition for a binary tree node
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        stack = []
+        current = root
+        
+        while current or stack:
+            while current:
+                stack.append(current)
+                current = current.left
+                
+            current = stack.pop()
+            result.append(current.val)
+            
+            current = current.right
+        
+        return result
+
+
+#225. Implement Stack using Queues
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+    def pop(self) -> int:
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
+
+
