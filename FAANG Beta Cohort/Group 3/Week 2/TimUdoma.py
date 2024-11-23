bcd9d6593a20daffc2041a33fe10d9885af31efa
Tim Udoma <timudoma@gmail.com>
2024-11-22 17:21:54 -0800

    add firstUniqChar soln

--- FAANG Beta Cohort/Group 3/Week 2/TimUdoma.py
+++ FAANG Beta Cohort/Group 3/Week 2/TimUdoma.py
@@ -17,4 +17,19 @@ class Solution:
                 if map[prev] != c: return False
 
         return not stack
+    
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        if not s:
+            return -1
+        
+        # Count character frequencies
+        char_count = Counter(s)
+        
+        # Check characters in original order 
+        for idx, char in enumerate(s):
+            if char_count[char] == 1:
+                return idx
+                
+        return -1
         
