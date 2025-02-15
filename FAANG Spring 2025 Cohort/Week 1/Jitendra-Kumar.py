103d0de60dbce4051141d2d674c010d2b6009e1f
j-kumar-3266-westcliff-edu <j.kumar.3266@westcliff.edu>
2025-02-14 18:58:34 -0800

    Update Jitendra-Kumar.py
    Eliminate Maximum Number of Monsters

--- FAANG Spring 2025 Cohort/Week 1/Jitendra-Kumar.py
+++ FAANG Spring 2025 Cohort/Week 1/Jitendra-Kumar.py
@@ -14,3 +14,19 @@ class Solution:
         return count
 # Time Complexity : O(n)
 # Space Complexity : O(1)
+
+#Eliminate Maximum Number of Monsters : https://leetcode.com/problems/eliminate-maximum-number-of-monsters/description
+class Solution:
+    def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
+        arrival = []
+        for i in range(len(dist)):
+            arrival.append(dist[i] / speed[i])
+        arrival.sort()
+        ans = 0
+        for i in range(len(arrival)):
+            if arrival[i] <= i:
+                break
+            ans += 1
+        return ans
+# Time Complexity : O(nlogn)
+# Space Complexity : O(n)
