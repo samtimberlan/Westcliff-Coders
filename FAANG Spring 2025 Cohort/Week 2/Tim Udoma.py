ab4b3ad864829a81567db3c200c6cca279521e3b
Tim <timudoma@gmail.com>
2025-02-21 16:01:53 -0800

    add second solution

--- FAANG Spring 2025 Cohort/Week 2/Tim Udoma.py
+++ FAANG Spring 2025 Cohort/Week 2/Tim Udoma.py
@@ -20,4 +20,25 @@ class Solution:
         return prev
     
     #2. 
-    
+    # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # fast and slow pointer. # slow will be mid. keep track of prev
+        if not head or not head.next: return None
+         # If only two nodes, delete the second one
+        if not head.next.next:
+            head.next = None
+            return head
+
+        fast, slow = head, head
+        while fast and fast.next:
+            fast = fast.next.next
+            prev = slow
+            slow = slow.next
+        prev.next, slow.next = slow.next, None
+        return head
+        
